b554d093000093af78a6834f118860c5b0bd22cf
Gene <g-nakagaki@ceres-inc.jp>
2022-02-18 09:19:38 +0900

    initial commit

---
+++ src/main/kotlin/com/genenakagaki/sample/solid/SolidApplication.kt
@@ -0,0 +1,11 @@
+package com.genenakagaki.sample.solid
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SolidApplication
+
+fun main(args: Array<String>) {
+	runApplication<SolidApplication>(*args)
+}
---
+++ src/test/kotlin/com/genenakagaki/sample/solid/SolidApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.genenakagaki.sample.solid
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SolidApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
