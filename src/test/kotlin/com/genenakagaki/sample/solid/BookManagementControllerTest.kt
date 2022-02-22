151804485e5f2d0f7c718ab699aafe3f1b6a053a
Gene <g-nakagaki@ceres-inc.jp>
2022-02-22 14:28:43 +0900

    add test

--- src/test/kotlin/com/genenakagaki/sample/solid/BookManagementControllerTest.kt
+++ src/test/kotlin/com/genenakagaki/sample/solid/BookManagementControllerTest.kt
@@ -1,5 +1,36 @@
 package com.genenakagaki.sample.solid
 
+import com.genenakagaki.sample.solid.repository.BookRepository
+import com.genenakagaki.sample.solid.repository.UserRepository
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
-internal class BookManagementControllerTest
+internal class BookManagementControllerTest {
+
+    @MockK
+    lateinit var bookRepository: BookRepository
+
+    @MockK
+    lateinit var userRepository: UserRepository
+
+    @InjectMockKs
+    lateinit var SUT: BookManagementController
+
+    @BeforeEach
+    fun init() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun hello() {
+        SUT.rentBook(mapOf(
+            "username" to "g-nakagaki",
+            "book_id" to "1",
+        ))
+    }
+}
